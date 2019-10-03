654a39857e4bf68127bf629fde844e8c145d3d59
Geordie Guy <geordie@gguy.me>
2019-10-03 14:49:36 +1000

    FTP server setup

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -1,3 +1,63 @@
-Install-WindowsFeature web-server -includemanagementtools
-Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
-Enable-PsRemoting -Force
+Install-WindowsFeature Web-FTP-Server -IncludeAllSubFeature
+Install-WindowsFeature Web-Server -IncludeAllSubFeature  IncludeManagementTools
+
+Import-Module WebAdministration
+
+$FTPSiteName = 'Default FTP Site'
+$FTPRootDir = 'c:\inetpub\wwwroot'
+$FTPPort = 21
+New-WebFtpSite -Name $FTPSiteName -Port $FTPPort -PhysicalPath $FTPRootDir
+
+$FTPUserGroupName = "FTP Users"
+$ADSI = [ADSI]"WinNT://$env:ComputerName"
+$FTPUserGroup = $ADSI.Create("Group", "$FTPUserGroupName")
+$FTPUserGroup.SetInfo()
+$FTPUserGroup.Description = "Members of this group can connect through FTP"
+$FTPUserGroup.SetInfo()
+
+$FTPUserName = "AzDevopsDeployUser"
+$FTPPassword = 'PurpleBankPassword123!'
+$CreateUserFTPUser = $ADSI.Create("User", "$FTPUserName")
+$CreateUserFTPUser.SetInfo()
+$CreateUserFTPUser.SetPassword("$FTPPassword")
+$CreateUserFTPUser.SetInfo()
+
+$UserAccount = New-Object System.Security.Principal.NTAccount("$FTPUserName")
+$SID = $UserAccount.Translate([System.Security.Principal.SecurityIdentifier])
+$Group = [ADSI]"WinNT://$env:ComputerName/$FTPUserGroupName,Group"
+$User = [ADSI]"WinNT://$SID"
+$Group.Add($User.Path)
+
+$FTPSitePath = "IIS:\Sites\$FTPSiteName"
+$BasicAuth = 'ftpServer.security.authentication.basicAuthentication.enabled'
+Set-ItemProperty -Path $FTPSitePath -Name $BasicAuth -Value $True
+
+$Param = @{
+    Filter   = "/system.ftpServer/security/authorization"
+    Value    = @{
+        accessType  = "Allow"
+        roles       = "$FTPUserGroupName"
+        permissions = 1
+    }
+    PSPath   = 'IIS:\'
+    Location = $FTPSiteName
+}
+Add-WebConfiguration @param
+
+$SSLPolicy = @(
+    'ftpServer.security.ssl.controlChannelPolicy',
+    'ftpServer.security.ssl.dataChannelPolicy'
+)
+Set-ItemProperty -Path $FTPSitePath -Name $SSLPolicy[0] -Value $false
+Set-ItemProperty -Path $FTPSitePath -Name $SSLPolicy[1] -Value $false
+
+$UserAccount = New-Object System.Security.Principal.NTAccount("$FTPUserGroupName")
+$AccessRule = [System.Security.AccessControl.FileSystemAccessRule]::new($UserAccount,
+    'ReadAndExecute',
+    'ContainerInherit,ObjectInherit',
+    'None',
+    'Allow'
+)
+$ACL = Get-Acl -Path $FTPRootDir
+$ACL.SetAccessRule($AccessRule)
+$ACL | Set-Acl -Path $FTPRootDir
